df4434529c1774c342d612cd7e0693e236c09f5e
Chandramohan Jayaraman <af84021@LC02C95Z1MD6R.US.AD.WELLPOINT.COM>
2020-10-18 19:40:08 -0400

    Asp.net core webapi versioning

---
+++ VersionApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace VersionApi.Controllers
+{
+    [ApiController]
+    //[Route("[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [MapToApiVersion("2.0")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ VersionApi/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Http;
+using System.Web.Http.Routing;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace EmployeeApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web api configuration and services - Register Constraint
+            var constraintResolver = new DefaultInlineConstraintResolver();
+            constraintResolver.ConstraintMap.Add("apiVersionConstraint", typeof(ApiVersionRouteConstraint));
+
+            // Web api routes
+            config.MapHttpAttributeRoutes(constraintResolver);
+        }
+    }
+}
